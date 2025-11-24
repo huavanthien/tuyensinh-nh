6850aba599c02a13361ea8e6099c299441501695
huavanthien <81952190+huavanthien@users.noreply.github.com>
2025-11-24 21:21:17 +0700

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,1 +1,12 @@
+// config/cloudinary.js
+const cloudinary = require('cloudinary').v2;
 
+cloudinary.config({
+  // Lấy từ biến môi trường
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true, // Khuyến nghị: Bật HTTPS cho URL ảnh
+});
+
+module.exports = cloudinary;
